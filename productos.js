6772596977c4f12872046078485ae2d95f9e72ad
eddygonzalez657 <eddygonzalez270712@gmail.com>
2026-05-24 13:26:14 -0600

    Primer commit

---
+++ productos.js
@@ -0,0 +1,27 @@
+const productos = [
+
+  {
+    nombre: "Cuaderno Profesional",
+    categoria: "cuadernos",
+    precio: "$45",
+    disponible: true,
+    imagen: "https://m.media-amazon.com/images/I/81xWQ3cs0fL.jpg"
+  },
+
+  {
+    nombre: "Cuaderno Colorido",
+    categoria: "cuadernos",
+    precio: "$23",
+    disponible: false,
+    imagen: "cuaderno2.jpg"
+  },
+
+  {
+    nombre: "Pluma Azul Bic",
+    categoria: "plumas",
+    precio: "$8",
+    disponible: true,
+    imagen: "https://upload.wikimedia.org/wikipedia/commons/5/56/Colored_pencils.jpg"
+  }
+
+];
